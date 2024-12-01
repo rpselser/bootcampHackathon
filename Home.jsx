b9cdba4dc14749d3697d0d42707bc8c6e41c9f32
SawB06 <157073273+SawB06@users.noreply.github.com>
2024-12-01 18:02:37 -0500

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,1 +1,40 @@
-// sawyer
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const Home = () => {
+
+    const NavButton = ({title,nav}) => {
+        const navigation = useNavigate();
+        return (
+            <button onClick={() => navigation(nav)}>
+                {title}
+            </button>
+        
+        );
+    };
+
+    const HomeHeader = () => {
+        return(
+            <h1 className='HomeHeader'>
+                AppDev Companion
+            </h1>
+        );
+    };
+
+    return(
+        <>
+            <HomeHeader />
+            <div id="links">
+                <NavButton title={"Announcements"} nav={"/announcements"} />
+                <NavButton title={"Attendance"} nav={"/attendance"} />
+                <NavButton title={"Discussion"} nav={"/announcements"} />
+                <NavButton title={"Materials"} nav={"/materials"} />
+                <NavButton title={"Grading"} nav={"/grading"} />
+                <NavButton title={"Homework"} nav={"/homework"} />
+            </div>
+        </>
+    );
+}
+
+export default Home;
